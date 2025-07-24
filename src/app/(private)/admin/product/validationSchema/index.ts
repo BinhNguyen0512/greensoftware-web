c07d31efe5ae3318c69c417a51983cdfcf1db5ad
Binh Nguyen <binh.nguyen@atech88.com>
2025-07-24 12:13:44 +0700

    fix: error message validations

--- src/app/(private)/admin/product/validationSchema/index.ts
+++ src/app/(private)/admin/product/validationSchema/index.ts
@@ -1,22 +1,25 @@
 import * as yup from "yup";
 
 export const validationSchema = yup.object().shape({
-  title: yup.string().trim().required("Vui lòng nhập tên sản phẩm"),
-  alias: yup.string().trim().required("Vui lòng nhập alias"),
+  title: yup.string().trim().required("Title is required"),
+  alias: yup.string().trim().required("Alias is required"),
   price: yup
     .number()
     .transform((currentValue, originalValue) =>
       originalValue === "" ? null : currentValue,
     )
-    .required("Vui lòng nhập số")
-    .min(1, "Giá phải lớn hơn 1"),
-  description: yup.string().trim().required("Vui lòng nhập chi tiết sản phẩm"),
-  shortDescription: yup.string().trim().required("Vui lòng mô tả ngắn"),
+    .required("Price is required")
+    .min(1, "Price must be greater than 0"),
+  description: yup.string().trim().required("Description is required"),
+  shortDescription: yup
+    .string()
+    .trim()
+    .required("Short description is required"),
   stock: yup
     .number()
     .transform((currentValue, originalValue) =>
       originalValue === "" ? null : currentValue,
     )
-    .required("Vui lòng nhập số")
-    .min(1, "Giá phải lớn hơn 1"),
+    .required("Stock is required")
+    .min(1, "Stock must be greater than 0"),
 });
